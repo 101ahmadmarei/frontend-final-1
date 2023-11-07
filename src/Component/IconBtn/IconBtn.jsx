f89eeb3cf9680f06df56c7ed2cdc7dbf3441f24b
Sura5 <surawg5@gmail.com>
2023-11-07 21:35:42 +0200

    Homepage

---
+++ src/Component/IconBtn/IconBtn.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { IconButton } from '@mui/material';
+
+
+export default function IconBtn({ icon }) {
+  return (
+
+    <IconButton >
+      {icon}
+    </IconButton>
+
+  );
+}
+
