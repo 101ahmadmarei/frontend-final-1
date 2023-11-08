b46feb9f1fdd3a329f5a9c043de0cb235387f860
amreshtiwi <ammr64@outlook.com>
2023-11-08 10:45:24 +0200

    resolve header conflict

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PersonalPage from './pages/PersonalPage/PersonalPage';
 import Bran from './Component/Brand/Bran';
 import Header from './Component/Header/Header';
 import NewArr from './Component/NewArrivals/NewArr';
+import MyOrdersPage from './pages/myOrdersPage/MyOrdersPage';
 
 
 
@@ -25,7 +26,8 @@ function App() {
       </div> */}
       {/* <PageHeader title={'Personal Information'}/> */}
       {/* <ProfileListItem/> */}
-      <PersonalPage/>
+      {/* <PersonalPage/> */}
+      <MyOrdersPage/>
     </ThemeProvider>
 
   );
